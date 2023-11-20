da8e7bfc3e88bba092f59c2d8d06f04ab350bd23
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2023-11-20 22:58:22 +0800

    Finished day-01

--- backend/day-01/tempCodeRunnerFile.py
+++ backend/day-01/tempCodeRunnerFile.py
@@ -3,4 +3,4 @@ age = 19
 isMale = True
 department = "Department of Software and Web Development ૮꒰ ˶• ༝ •˶꒱ა ♡"
 
-print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
