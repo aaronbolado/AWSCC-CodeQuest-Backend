07500b837b43fbebdede10d00255c4da0e01797f
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2023-11-20 23:08:46 +0800

    Removed an extra file

--- backend/day-01/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-name = "Aaron John C. Bolado"
-age = 19
-isMale = True
-department = "Department of Software and Web Development ૮꒰ ˶• ༝ •˶꒱ა ♡"
-
-print(f"{name} is an {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
