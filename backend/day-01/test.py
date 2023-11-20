eb9bdcacf5006e7a1db38a22e715becff3159437
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2023-11-20 23:07:50 +0800

    Update day-01

--- backend/day-01/test.py
+++ backend/day-01/test.py
@@ -1,6 +1,8 @@
 name = "Aaron John C. Bolado"
 age = 19
 isMale = True
-department = "Department of Software and Web Development ૮꒰ ˶• ༝ •˶꒱ა ♡"
+department = "Department of Software and Web Development"
+aboutYou = "I am a 2nd year computer science student studying full stack web development"
 
-print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding.")
+print(f"{name} is a {age}-year old {'male' if isMale else 'female'} participating in {department}'s 30 Days of Coding. I love {department} and I love being in the Backend even more!")
+print(f"One thing about me is that {aboutYou}.")
