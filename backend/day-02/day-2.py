e0ca01f6afdd48deb37d7aff201c6cea727372fb
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2023-11-22 12:25:55 +0800

    Created program for adding integer inputs

---
+++ backend/day-02/day-2.py
@@ -0,0 +1,5 @@
+num1 = input("\nEnter the first number: ")
+num2 = input("Enter the second number: ")
+num3 = input("Enter the third number: ")
+
+print(f"\nSum: {int(num1) + int(num2) + int(num3)}\n")
