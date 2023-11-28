1afbf4df655a86778535b950cd62fdc477f7c3a1
aaronbolado <aaronjohncbolado@iskolarngbayan.pup.edu.ph>
2023-11-28 19:40:11 +0800

    Finished fizzbuzz

---
+++ backend/day-06/challenge.py
@@ -0,0 +1,15 @@
+
+limit = int(input("\nLimit: "))
+i = 1
+
+print() # new line
+while (i <= limit):
+    if (i % 3 == 0 and i % 5 == 0):
+        print("FizzBuzz!")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    i+=1
